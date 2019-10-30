a9bf767c0219bbd4d89b813757edb13aeafa3b1c
zyongan <yonganzhong@outlook.com>
2019-10-30 15:49:24 +0800

    improve parameter

--- Inception_v1.py
+++ Inception_v1.py
@@ -9,9 +9,9 @@
 import os
 import numpy as np
 import tensorflow as tf
-
+import tensorflow.contrib.slim as slim
 # # generates a truncated normal distribution
-# trunc_normal = lambda stddev: tf.truncated_normal_initializer(mean=0.0, stddev=stddev)
+# trunc_normal = lambda stddev: tf.random.truncated_normal _initializer(mean=0.0, stddev=stddev)
 
 class InceptionV1():
     """
@@ -24,21 +24,20 @@ class InceptionV1():
         self.decay_steps = decay_steps
         self.decay_rate = decay_rate
         self.learning_rate = learning_rate
-        # self.optimizer = optimizer
         self.keep_prob = keep_prob
         self.global_pool = global_pool
         self.spacial_squeeze = spacial_squeeze
         # self.initializer = tf.random_normal_initializer(stddev=0.1)
         # add placeholder (X,label)
-        self.raw_input_data = tf.placeholder(tf.float32, shape=[None, input_shape[0], input_shape[1], input_shape[2]], name="input_images")
+        self.raw_input_data = tf.compat.v1.placeholder (tf.float32, shape=[None, input_shape[0], input_shape[1], input_shape[2]], name="input_images")
         # y [None,num_classes]
-        self.raw_input_label = tf.placeholder(tf.float32, shape=[None, self.num_classes], name="class_label")
+        self.raw_input_label = tf.compat.v1.placeholder (tf.float32, shape=[None, self.num_classes], name="class_label")
 
         self.global_step = tf.Variable(0, trainable=False, name="global_step")
         self.epoch_step = tf.Variable(0, trainable=False, name="epoch_step")
 
-        # # logits
-        # self.logits =  self.inference(input_op=self.raw_input_data, name='inference')
+        # logits
+        self.logits =  self.inference(self.raw_input_data)
         # # computer loss value
         # self.loss = self.losses(labels=self.raw_input_label, logits=self.logits, name='loss')
         # # train operation
@@ -46,7 +45,7 @@ class InceptionV1():
         # self.evaluate_accuracy = self.evaluate_batch(self.logits, self.raw_input_label) / batch_size
 
 
-    def inference(self):
+    def inference(self, inputs):
        """
        Inception V1 net structure
        :param input_op:
@@ -54,7 +53,7 @@ class InceptionV1():
        :return:
        """
        self.prameter = []
-       prop =  self.inception_v1(inputs = self.raw_input_data,
+       prop =  self.inception_v1(inputs = inputs,
                                  num_classes = self.num_classes,
                                  keep_prob = self.keep_prob,
                                  global_pool = self.global_pool,
@@ -75,13 +74,13 @@ class InceptionV1():
         :param spatial_squeeze:
         :return:
         """
-        with tf.variable_scope(scope, default_name='InceptionV1', values=[inputs]) as scope:
+        with tf.compat.v1.variable_scope(scope, default_name='InceptionV1', values=[inputs]) as scope:
             net = self.inception_v1_base(inputs=inputs, scope=scope)
-            with tf.variable_scope('logits'):
+            with tf.compat.v1.variable_scope('logits'):
                 if global_pool:
                     net = tf.reduce_mean(net, [1, 2], keep_dims=True, name='Global_Pool')
                 else:
-                    net = avgPool2dLayer(input_op=net, scope='AvgPool_0a_7x7', kernel_size=[7, 7], strides=1)
+                    net = avgPool2dLayer(input_op=net, scope='AvgPool_0a_7x7', kernel_size=[7, 7], strides=1, padding='VALID')
                 # dropout
                 net = dropoutLayer(input_op=net, scope='Dropout_0b', keep_prob=keep_prob)
                 # conv layer 1*1*num_class
@@ -91,7 +90,7 @@ class InceptionV1():
                 if spatial_squeeze:
                     logits = tf.squeeze(input=logits, axis=[1, 2], name='SpatialSqueeze')
                 prop = softmaxLayer(input_op=logits, scope='Softmax')
-        return prop
+        return logits
 
 
     def inception_v1_base(self, inputs, scope='InceptionV1'):
@@ -99,7 +98,7 @@ class InceptionV1():
         Inception V1 base architecture
         :return:
         """
-        with tf.variable_scope(scope, default_name='InceptionV1', values=[inputs]):
+        with tf.compat.v1.variable_scope(scope, default_name='InceptionV1', values=[inputs]):
             # conv_7*7*64
             net = conv2dLayer(input_op=inputs, scope='Conv2d_1a_7x7', filters=64, kernel_size=[7, 7], strides=2)
 
@@ -111,7 +110,7 @@ class InceptionV1():
             net = conv2dLayer(input_op=net, scope='Conv2d_2c_3x3', filters=192, kernel_size=[3, 3], strides=1)
 
             # max pool 3*3
-            net = maxPool2dLayer(input_op=net, scope='MaxPool_3a_3x3', kernel_size=[3, 3], strides=1)
+            net = maxPool2dLayer(input_op=net, scope='MaxPool_3a_3x3', kernel_size=[3, 3], strides=2)
             # inception module
             net = inception_module_v1(input_op=net, scope='Mixed_3b', filters_list=[64, 96, 128, 16, 32, 32])
             # inception module
@@ -119,7 +118,7 @@ class InceptionV1():
             # inception module
 
             # max pool 3*3
-            net = maxPool2dLayer(input_op=net, scope='MaxPool_4a_3x3', kernel_size=[3, 3], strides=1)
+            net = maxPool2dLayer(input_op=net, scope='MaxPool_4a_3x3', kernel_size=[3, 3], strides=2)
             # inception module
             net = inception_module_v1(input_op=net, scope='Mixed_4b', filters_list=[192, 92, 208, 16, 48, 64])
             # inception module
@@ -132,13 +131,20 @@ class InceptionV1():
             net = inception_module_v1(input_op=net, scope='Mixed_4f', filters_list=[256, 160, 320, 32, 128, 128])
 
             # max pool 2*2
-            net = maxPool2dLayer(input_op=net, scope='MaxPool_5a_2x2', kernel_size=[2, 2], strides=1)
+            net = maxPool2dLayer(input_op=net, scope='MaxPool_5a_2x2', kernel_size=[2, 2], strides=2)
             # inception module
             net = inception_module_v1(input_op=net, scope='Mixed_5b', filters_list=[256, 160, 320, 32, 128, 128])
             # inception module
             net = inception_module_v1(input_op=net, scope='Mixed_5c', filters_list=[384, 192, 384, 48, 128, 128])
 
             return net
+
+    def fill_feed_dict(self, image_feed, label_feed):
+        feed_dict = {
+            self.raw_input_data: image_feed,
+            self.raw_input_label: label_feed
+        }
+        return feed_dict
 
 
 def inception_module_v1(input_op, filters_list, scope):
@@ -152,32 +158,34 @@ def inception_module_v1(input_op, filters_list, scope):
     :param scope:
     :return:
     """
-    with tf.variable_scope(scope):
+    with tf.compat.v1.variable_scope(scope):
         # branch0
-        with tf.variable_scope('Branch_0'):
+        with tf.compat.v1.variable_scope('Branch_0'):
             branch_0 = conv2dLayer(input_op=input_op, scope='Conv2d_0a_1x1', filters=filters_list[0],
                                  kernel_size=[1, 1], strides=1)
         # branch 1
-        with tf.variable_scope('Branch_1'):
+        with tf.compat.v1.variable_scope('Branch_1'):
             branch_1 = conv2dLayer(input_op=input_op, scope='Conv2d_0a_1x1', filters=filters_list[1],
                                  kernel_size=[1, 1], strides=1)
             branch_1 = conv2dLayer(input_op=branch_1, scope='Conv2d_0b_3x3', filters=filters_list[2],
                                  kernel_size=[3, 3], strides=1)
         # branch 2
-        with tf.variable_scope('Branch_2'):
+        with tf.compat.v1.variable_scope('Branch_2'):
             branch_2 = conv2dLayer(input_op=input_op, scope='Conv2d_0a_1x1', filters=filters_list[3],
                                  kernel_size=[1, 1], strides=1)
             branch_2 = conv2dLayer(input_op=branch_2, scope='Conv2d_0b_3x3', filters=filters_list[4],
                                  kernel_size=[3, 3], strides=1)
         # branch 3
-        with tf.variable_scope('Branch_3'):
+        with tf.compat.v1.variable_scope('Branch_3'):
             branch_3 = maxPool2dLayer(input_op=input_op, scope='MaxPool_0a_3x3', kernel_size=[3, 3], strides=1)
             branch_3 = conv2dLayer(input_op=branch_3, scope='Conv2d_0b_3x3', filters=filters_list[5],
                                  kernel_size=[3, 3], strides=1)
-        # concat
+        # concat branch
         output_op = tf.concat(values=[branch_0, branch_1, branch_2, branch_3], axis=3, name='Concat')
 
     return output_op
+
+
 
 
 
@@ -208,9 +216,9 @@ def conv2dLayer(input_op, scope, filters, kernel_size, strides, use_bias=False, 
         if use_bias:
             biases = getBias(bias_shape=[filters])
             outputs = tf.nn.bias_add(value=outputs, bias=biases)
-            parameter += [filter, biases]
-        else:
-            parameter += [filters]
+        #     parameter += [filter, biases]
+        # else:
+        #     parameter += [filters]
 
         return tf.nn.relu(outputs)
 
@@ -229,7 +237,7 @@ def fcLayer(input_op, scope, filters, parameter):
     with tf.name_scope(scope) as scope:
         weights = getFCWeight(weight_shape=[features, filters])
         biases = getBias(bias_shape=[filters])
-        parameter += [weights, biases]
+        # parameter += [weights, biases]
         return tf.nn.relu_layer(x=input_op, weights=weights, biases=biases)
 
 
@@ -243,7 +251,7 @@ def maxPool2dLayer(input_op, scope, kernel_size=None, strides=None, padding='SAM
     :param padding:
     :return:
     """
-    with tf.variable_scope(scope) as scope:
+    with tf.compat.v1.variable_scope(scope) as scope:
         if kernel_size is None:
             ksize = [1, 2, 2, 1]
         else:
@@ -252,16 +260,16 @@ def maxPool2dLayer(input_op, scope, kernel_size=None, strides=None, padding='SAM
             strides = [1, 2, 2, 1]
         else:
             strides = [1, strides, strides, 1]
-        return tf.nn.max_pool(value=input_op, ksize=ksize, strides=strides, padding=padding, name='MaxPool')
+        return tf.nn.max_pool2d(input=input_op, ksize=ksize, strides=strides, padding=padding, name='MaxPool')
 
 
-def avgPool2dLayer(input_op, scope, kernel_size=None, strides=None):
+def avgPool2dLayer(input_op, scope, kernel_size=None, strides=None, padding='SAME'):
     """
     average_pool pooling layer
     :param input_op:
     :return:
     """
-    with tf.variable_scope(scope) as scope:
+    with tf.compat.v1.variable_scope(scope) as scope:
         if kernel_size is None:
             ksize = [1, 2, 2, 1]
         else:
@@ -270,7 +278,7 @@ def avgPool2dLayer(input_op, scope, kernel_size=None, strides=None):
             strides = [1, 2, 2, 1]
         else:
             strides = [1, strides, strides, 1]
-        return tf.nn.avg_pool(value=input_op, ksize=ksize, strides=strides, name='AvgPool')
+        return tf.nn.avg_pool2d(value=input_op, ksize=ksize, strides=strides, padding=padding, name='AvgPool')
 
 
 def flatten(input_op, scope):
@@ -279,7 +287,7 @@ def flatten(input_op, scope):
     :param input_op:
     :return:
     """
-    with tf.variable_scope(scope) as scope:
+    with tf.compat.v1.variable_scope(scope) as scope:
         shape = input_op.get_shape().as_list()
         out_dim = 1
         for d in shape[1:]:
@@ -295,8 +303,8 @@ def dropoutLayer(input_op, scope, keep_prob):
     :param keep_prob:
     :return:
     """
-    with tf.variable_scope(scope) as scope:
-        return tf.nn.dropout(input_op, keep_prob=keep_prob, name='Dropout')
+    with tf.compat.v1.variable_scope(scope) as scope:
+        return tf.nn.dropout(input_op, rate=1-keep_prob, name='Dropout')
 
 
 def softmaxLayer(input_op, scope):
@@ -307,7 +315,7 @@ def softmaxLayer(input_op, scope):
     :param n_class:
     :return:
     """
-    with tf.variable_scope(scope):
+    with tf.compat.v1.variable_scope(scope):
         return tf.nn.softmax(logits=input_op, name='Softmax')
 
 
@@ -317,7 +325,7 @@ def getConvFilter(filter_shape):
     :param filter_shape:
     :return:
     """
-    return tf.Variable(initial_value=tf.truncated_normal(shape=filter_shape, mean=0.0, stddev=0.01, dtype=tf.float32),
+    return tf.Variable(initial_value=tf.random.truncated_normal(shape=filter_shape, mean=0.0, stddev=0.01, dtype=tf.float32),
                        trainable=True, name='Filter')
 
 
@@ -327,14 +335,13 @@ def getFCWeight(weight_shape):
     :param weight_shape:
     :return:
     """
-    return tf.Variable(initial_value=tf.truncated_normal(shape=weight_shape, mean=0.0, stddev=0.01, dtype=tf.float32),
+    return tf.Variable(initial_value=tf.random.truncated_normal(shape=weight_shape, mean=0.0, stddev=0.01, dtype=tf.float32),
                        trainable=True, name='Weight')
 
 
 def getBias(bias_shape):
     return tf.Variable(initial_value=tf.constant(value=0.0, shape=bias_shape, dtype=tf.float32),
                        trainable=True, name='Bias')
-
 
 
 
