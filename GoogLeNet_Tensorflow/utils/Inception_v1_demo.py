7df9519660988da43157109fb4fd89b63756de4d
alexchung <yonganzhong@outlook.com>
2020-05-13 10:21:04 +0800

    add inception v1 test module

--- GoogLeNet_Tensorflow/utils/Inception_v1_demo.py
+++ GoogLeNet_Tensorflow/utils/Inception_v1_demo.py
@@ -8,19 +8,19 @@
 import numpy as np
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
-from tensorflow.contrib.slim.python.slim.nets import inception_v3
+from tensorflow.contrib.slim.python.slim.nets import inception_v1
 
-pretrain_model_dir = '/home/alex/Documents/pretraing_model/Inception/inception_v3/inception_v3.ckpt'
+pretrain_model_dir = '/home/alex/Documents/pretrain_model/Inception/inception_v1/inception_v1.ckpt'
 
 
-images = tf.Variable(initial_value=tf.random_uniform(shape=(5, 299, 299, 3), minval=0, maxval=3), dtype=tf.float32)
+images = tf.Variable(initial_value=tf.random_uniform(shape=(5, 224, 224, 3), minval=0, maxval=3), dtype=tf.float32)
 class_num = tf.constant(value=5, dtype=tf.int32)
 # is_training = True
 
 
 # read net
-with slim.arg_scope(inception_v3.inception_v3_arg_scope()):
-    logits, end_points = inception_v3.inception_v3(images)
+with slim.arg_scope(inception_v1.inception_v1_arg_scope()):
+    logits, end_points = inception_v1.inception_v1(images, num_classes=1001)
 
 if __name__ == "__main__":
 
@@ -29,12 +29,12 @@ if __name__ == "__main__":
     with tf.Session() as sess:
         # images, class_num = sess.run([images, class_num])
         sess.run(init)
-        for var in tf.model_variables():
+        for var in tf.compat.v1.model_variables():
             print(var.name)
 
         # exclusion scope
-        exclude_variable = ['InceptionV3/Logits',
-                      'InceptionV3/AuxLogits']
+        exclude_variable = None
+        # exclude_variable = ['InceptionV1/Logits/Conv2d_0c_1x1']
 
         variable = slim.get_variables_to_restore(exclude=exclude_variable)
         # assign specific variables from a checkpoint
@@ -50,7 +50,7 @@ if __name__ == "__main__":
                                                   ignore_missing_vars=True)
         # restore variable to sess
         assign_fn(sess)
-        print(sess.run('InceptionV3/Conv2d_1a_3x3/weights:0'))
+        print(sess.run('InceptionV1/Logits/Conv2d_0c_1x1/weights:0'))
 
 
 
