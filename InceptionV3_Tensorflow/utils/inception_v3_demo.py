9ef7e7d51415faea4abdecfaa0646a16e4d996d8
alexchung <yonganzhong@outlook.com>
2020-05-13 10:17:30 +0800

    rename module

---
+++ InceptionV3_Tensorflow/utils/inception_v3_demo.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#------------------------------------------------------
+# @ File       : inception_v3_demo.py
+# @ Description:  
+# @ Author     : Alex Chung
+# @ Contact    : yonganzhong@outlook.com
+# @ License    : Copyright (c) 2017-2018
+# @ Time       : 2020/5/13 上午10:01
+# @ Software   : PyCharm
+#-------------------------------------------------------
